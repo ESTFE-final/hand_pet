cf6e10d37a4c36a79fa562f547a57e86dd06e929
Siwon <inggo113@naver.com>
2024-10-11 13:50:54 +0900

    complete

---
+++ src/components/LoginComponents/logincontent.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// 컨텐츠 감싸는 컴포넌트
+export const ContentWrap = styled.div`
+	margin-top: 26px;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;  /* 중앙 정렬 */
+	align-items: center;  /* 가로 중앙 정렬 */
+`;
