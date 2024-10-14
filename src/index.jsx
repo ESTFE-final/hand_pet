7b55b6d4be4b7ba3f144c81b2b9778c376e7da25
Kim Min Sang <sp4593@naver.com>
2024-10-14 22:30:06 +0900

    현재까지 완료한 내용에서 시작

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
 // 풀리퀘스트를 위한 변경사항 만들기 주석 추가
