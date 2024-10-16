cc20246914c9accb5550ab3ef5eb47156be79632
“lbyul” <rlaquf7737@naver.com>
2024-10-17 01:51:23 +0900

    Feat: 이미지 업로드 프리뷰 및 삭제 기능 구현

--- src/pages/PostUploadPage.jsx
+++ src/pages/PostUploadPage.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import axios from 'axios';
 import { NavigationBar } from '../components/SharedComponents/CommonComponents';
 import uploadIcon from '../assets/icons/upload-file.svg';
+import imageCloseIcon from '../assets/icons/icon-image-close.svg';
 
 const PostUploadWrapper = styled.section`
 	display: flex;
@@ -22,6 +23,11 @@ const UploadButton = styled.button`
 	padding: 15px 50px;
 	border-radius: 64px;
 	font-size: 2.8rem;
+	transition: all 0.3s ease;
+
+	&:hover {
+		background-color: var(--primary);
+	}
 `;
 
 const PostUploadContainer = styled.div`
@@ -29,16 +35,18 @@ const PostUploadContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	position: relative;
+	padding: 0 32px;
+	padding-right: ${(props) => (props.hasImages ? '0' : '32px')};
 `;
 
 const PostTextDiv = styled.div`
-	width: calc(100% - 64px);
-	margin: 0 32px;
+	width: calc(100% - 32px);
 	min-height: 150px;
 	font-size: 2.8rem;
 	border: none;
 	outline: none;
 	overflow: auto;
+	margin-bottom: 32px;
 
 	&:empty:before {
 		content: attr(data-placeholder);
@@ -46,6 +54,52 @@ const PostTextDiv = styled.div`
 	}
 `;
 
+// 이미지 프리뷰
+const ImagePreviewContainer = styled.div`
+	width: ${(props) => (props.isSingle ? 'calc(100% - 32px)' : '100%')};
+	height: ${(props) => (props.isSingle ? '492px' : '330px')};
+	overflow-x: auto;
+	overflow-y: hidden;
+	white-space: nowrap;
+	display: flex;
+	gap: 15px;
+	margin-right: ${(props) => (props.isSingle ? '0' : '32px')};
+
+	&::-webkit-scrollbar {
+		display: none;
+	}
+
+	-ms-overflow-style: none;
+	scrollbar-width: none;
+`;
+
+const ImagePreviewWrapper = styled.div`
+	position: relative;
+	height: 100%;
+	width: ${(props) => (props.isSingle ? '100%' : '330px')};
+	flex-shrink: 0;
+	margin-right: ${(props) => (props.isLast && !props.isSingle ? '32px' : '0')};
+`;
+
+const ImagePreview = styled.img`
+	height: 100%;
+	width: 100%;
+	object-fit: cover;
+	border-radius: 20px;
+`;
+
+// 이미지 삭제
+const DeleteImageButton = styled.button`
+	background: url(${imageCloseIcon}) no-repeat;
+	background-size: 100%;
+	width: 46px;
+	height: 46px;
+	position: absolute;
+	top: 12px;
+	right: 12px;
+`;
+
+// 업로드 파일
 const UploadFileContainer = styled.div`
 	position: absolute;
 	bottom: 32px;
@@ -67,9 +121,28 @@ const UploadFileInput = styled.input`
 
 const PostUploadPage = () => {
 	const [content, setContent] = useState('');
+	const [images, setImages] = useState([]);
 
 	const handleInput = (e) => {
 		setContent(e.target.innerText);
+	};
+
+	const handleImageUpload = (e) => {
+		const files = Array.from(e.target.files);
+		const newImages = files.map((file) => ({
+			file,
+			preview: URL.createObjectURL(file),
+		}));
+		setImages((prevImages) => [...prevImages, ...newImages]);
+	};
+
+	const handleDeleteImage = (index) => {
+		setImages((prevImages) => {
+			const newImages = [...prevImages];
+			URL.revokeObjectURL(newImages[index].preview);
+			newImages.splice(index, 1);
+			return newImages;
+		});
 	};
 
 	return (
@@ -78,15 +151,39 @@ const PostUploadPage = () => {
 				title={'게시글 올리기'}
 				rightButton={<UploadButton>업로드</UploadButton>}
 			/>
-			<PostUploadContainer>
+
+			<PostUploadContainer hasImages={images.length > 0}>
 				<PostTextDiv
 					contentEditable
 					onInput={handleInput}
 					data-placeholder="게시글을 입력하세요.."
 				/>
+				{images.length > 0 && (
+					<ImagePreviewContainer isSingle={images.length === 1}>
+						{images.map((img, index) => (
+							<ImagePreviewWrapper
+								key={index}
+								isSingle={images.length === 1}
+								isLast={index === images.length - 1}
+							>
+								<ImagePreview src={img.preview} alt={`Preview ${index + 1}`} />
+								<DeleteImageButton
+									onClick={() => handleDeleteImage(index)}
+									aria-label="이미지 추가 취소하기"
+								/>
+							</ImagePreviewWrapper>
+						))}
+					</ImagePreviewContainer>
+				)}
 				<UploadFileContainer>
 					<UploadFileLabel htmlFor="uploadimg" />
-					<UploadFileInput type="file" id="uploadimg" accept="image/*" />
+					<UploadFileInput
+						type="file"
+						id="uploadimg"
+						accept="image/*"
+						onChange={handleImageUpload}
+						multiple
+					/>
 				</UploadFileContainer>
 			</PostUploadContainer>
 		</PostUploadWrapper>
