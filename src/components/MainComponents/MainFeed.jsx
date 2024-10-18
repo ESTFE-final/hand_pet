c44469eed1848f1e8b5702dc293666ad993308f0
Siwon <inggo113@naver.com>
2024-10-18 09:54:25 +0900

    fix#Mainfeed

--- src/components/MainComponents/MainFeed.jsx
+++ src/components/MainComponents/MainFeed.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import FeedItem from './FeedItem'; // FeedItem 컴포넌트 import
 
-const MainFeed = ({ posts }) => { // posts props 추가
-    return (
-        <>
-            {posts.map((post, index) => ( // posts 배열을 순회하며 FeedItem 렌더링
-                <FeedItem key={index} content={post.content} postImgSrc={post.image} /> // 이미지 속성은 실제 데이터에 따라 수정
-            ))}
-        </>
-    );
+const MainFeed = ({ posts, onPostClick }) => { // onPostClick props 추가
+  return (
+    <>
+      {posts.map((post) => ( // posts 배열을 순회하며 FeedItem 렌더링
+        <FeedItem 
+          key={post.id} // ID를 키로 사용
+          content={post.content} 
+          postImgSrc={post.image} 
+          onClick={() => onPostClick(post.id)} // 클릭 핸들러 추가
+        />
+      ))}
+    </>
+  );
 };
 
 export default MainFeed;
