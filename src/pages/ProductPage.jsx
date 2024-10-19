59c95f4e24f39c44062a63fbfa0878687ce91a65
jay <jaeyoung6720@naver.com>
2024-10-19 12:08:50 +0900

    feature / 공통 컴포넌트 AlertModal props 수정 및 상품 삭제 기능 구현 완료

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import {
 	ProductImage,
 	ProductName,
@@ -12,7 +13,10 @@ import ProductButton from '../components/ProductComponents/ProductButton';
 import QuantitySelector from '../components/ProductComponents/QuantitySelector';
 import TotalPrice from '../components/ProductComponents/TotalPrice';
 import { NavigationBar } from '../components/SharedComponents/CommonComponents';
-import { PostModal } from '../components/SharedComponents/CommonComponents';
+import {
+	PostModal,
+	AlertModal,
+} from '../components/SharedComponents/CommonComponents';
 import RightmenuIcon from '../assets/icons/icon-vertical-color.svg';
 
 const Container = styled.div`
@@ -42,7 +46,11 @@ const ProductPage = () => {
 	const [quantity, setQuantity] = useState(1);
 	const token = localStorage.getItem('authToken');
 	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
 	const [modalOptions, setModalOptions] = useState([]);
+	const [alertText, setAlertText] = useState('');
+	const [alertButtonAction, setAlertButtonAction] = useState(() => () => {});
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const fetchProductDetail = async () => {
@@ -81,6 +89,52 @@ const ProductPage = () => {
 		}
 	};
 
+	const openModal = (options) => {
+		setModalOptions(options);
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
+	const openAlertModal = (text, action) => {
+		setAlertText(text);
+		setAlertButtonAction(() => action);
+		setIsAlertModalOpen(true);
+	};
+
+	const closeAlertModal = () => {
+		setIsAlertModalOpen(false);
+	};
+
+	const rightBtnClick = () => {
+		openModal([
+			{ text: '상품 편집', onClick: () => {} },
+			{
+				text: '상품 삭제',
+				onClick: () =>
+					openAlertModal('정말로 이 상품을 삭제하시겠습니까?', async () => {
+						try {
+							await axios.delete(
+								`https://estapi.mandarin.weniv.co.kr/product/${product_id}`,
+								{
+									headers: {
+										Authorization: `Bearer ${token}`,
+										'Content-type': 'application/json',
+									},
+								}
+							);
+							alert('상품이 삭제되었습니다.');
+							navigate('/profile');
+						} catch (error) {
+							console.error('상품 삭제 실패:', error);
+							alert('상품 삭제에 실패했습니다.');
+						}
+					}),
+			},
+		]);
+	};
 	if (error) {
 		return (
 			<Container>
@@ -98,22 +152,6 @@ const ProductPage = () => {
 	}
 
 	const totalPrice = product.price * quantity;
-
-	const openModal = (options) => {
-		setModalOptions(options);
-		setIsModalOpen(true);
-	};
-
-	const closeModal = () => {
-		setIsModalOpen(false);
-	};
-
-	const rightBtnClick = () => {
-		openModal([
-			{ text: '상품 편집', onClick: () => {} },
-			{ text: '상품 삭제', onClick: () => {} },
-		]);
-	};
 
 	return (
 		<>
@@ -143,6 +181,13 @@ const ProductPage = () => {
 					isOpen={isModalOpen}
 					onClose={closeModal}
 					options={modalOptions}
+				/>
+				<AlertModal
+					modalShow={isAlertModalOpen}
+					alertText={alertText}
+					buttonText="확인"
+					modalClose={closeAlertModal}
+					buttonAction={alertButtonAction}
 				/>
 				<ProductButton />
 			</Container>
