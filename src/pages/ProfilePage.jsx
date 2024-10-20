e06b195d5418ed0e3b10504db2f3abc538e11f8a
jay <jaeyoung6720@naver.com>
2024-10-20 09:43:05 +0900

    feature / 메인페이지 상품 카테고리 별 분류 및 가격 순 분류 기능 구현 완료

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
 import Profile from '../components/ProfileComponents/Profile';
