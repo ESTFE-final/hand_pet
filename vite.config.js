baba800e19e361a85eddca294256e9686aa11672
jay <jaeyoung6720@naver.com>
2024-10-22 00:38:11 +0900

    feature / dist 경로 재설정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		rollupOptions: {
-			input: './public/index.html', // public 폴더의 index.html을 엔트리로 설정
-		},
-	},
 });
